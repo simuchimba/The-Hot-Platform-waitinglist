35db6bf9ab24cecee2dc7732616fee3007761131
Kuunda Simuchimba <109965577+simuchimba@users.noreply.github.com>
2023-10-25 01:05:38 +0200

    Update JoinWaitlist.tsx

--- src/components/JoinWaitlist.tsx
+++ src/components/JoinWaitlist.tsx
@@ -98,7 +98,7 @@ export default function JoinWaitlist() {
                 <br/>
 
                 <h5>
-                    Professional Accountss
+                    Professional Accounts
                     <br/>
                 </h5>
 
